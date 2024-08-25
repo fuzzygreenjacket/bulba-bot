bd5633a05dbb66d0bbc57e05b77ada9f091eb779
fuzzygreenjacket <matthewlhsu@gmail.com>
2024-08-24 17:40:41 -0700

    Bot setup
    created bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+# bot.py
+import os
+import random
+import discord
+from dotenv import load_dotenv
+
+# 1
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# 2
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(TOKEN)
