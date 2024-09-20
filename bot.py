7c930d673b1bf949f4cf22be997590bb0ae72eb9
fuzzygreenjacket <matthewlhsu@gmail.com>
2024-09-20 16:46:58 -0700

    new file for roll.py
    Used cogs + extensions to separate roll command in its own file

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 # bot.py
 import os
-import random
 import discord
 from dotenv import load_dotenv
-
 from discord.ext import commands
 
 load_dotenv()
@@ -14,17 +12,7 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix='-')
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-
-@bot.command(name='roll')
-async def roll(ctx, num_dice: int, num_sides: int):
-    response = 0
-    for i in range(num_dice):
-        response += random.randint(1, num_sides)
-    await ctx.send(response)
-
-@roll.error
-async def roll_error(ctx, error):
-    await ctx.send("Error! Make sure you are specifying both the number of dice and the number of sides (i.e. `-roll 2 6`).")
+    await bot.load_extension("roll")
 
 @bot.command(name='give-role')
 @commands.has_permissions(administrator=True)
---
+++ roll.py
@@ -0,0 +1,21 @@
+import discord
+import random
+from discord.ext import commands
+
+class Roll(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(name='roll')
+    async def roll(self, ctx, num_dice: int, num_sides: int):
+        response = 0
+        for i in range(num_dice):
+            response += random.randint(1, num_sides)
+        await ctx.send(response)
+    
+    @roll.error
+    async def roll_error(self, ctx, error):
+        await ctx.send("Error! Make sure you are specifying both the number of dice and the number of sides (i.e. `-roll 2 6`).")
+
+async def setup(bot):
+    await bot.add_cog(Roll(bot))
